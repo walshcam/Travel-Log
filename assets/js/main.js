1789758423d82be5e2ae5667429d00a70974a138
walshcam <35083171+walshcam@users.noreply.github.com>
2018-04-12 17:02:25 -0400

    added moment.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,21 +17,21 @@ let userFlightQuery = {
     origin: "BOS",
     destination: "LAX",
     departureDate: "2018-06-25",
-    returnDate: "2018-4-15",
+    returnDate: "2018-07-15",
     durationStart: "5",
     durationEnd: "7",
-    maxPrice: "900",
+    maxPrice: "2000",
     currency: "USD",
     adults: "1",
     children: "2",
     infants: "1",
-    nonstop: true,
+    nonstop: "true",
     travelClass: "ECONOMY",
     numberOfResults: "5",
 
     //function to create a request URL for the Amadeus API
     flightQuery: function() {
-        var queryURL= `https://api.sandbox.amadeus.com/v1.2/flights/low-fare-search?apikey=p1YNpUhekCL0mknNyltGFNg8AAsc5yRI&origin=${userFlightQuery.origin}&destination=${userFlightQuery.destination}&departure_date=${this.departureDate}&return_date=2018-07-10&adults=2&children=1&infants=1&nonstop=true&max_price=980&currency=USD&travel_class=ECONOMY&number_of_results=5`;
+        var queryURL= `https://api.sandbox.amadeus.com/v1.2/flights/low-fare-search?apikey=p1YNpUhekCL0mknNyltGFNg8AAsc5yRI&origin=${userFlightQuery.origin}&destination=${userFlightQuery.destination}&departure_date=${userFlightQuery.departureDate}&return_date=${userFlightQuery.returnDate}&adults=${userFlightQuery.adults}&children=${userFlightQuery.children}&infants=${userFlightQuery.infants}&nonstop=${userFlightQuery.nonstop}&max_price=${userFlightQuery.maxPrice}&currency=${userFlightQuery.currency}&travel_class=${userFlightQuery.travelClass}&number_of_results=${userFlightQuery.numberOfResults}`;
         // var queryURL = `https://api.sandbox.amadeus.com/v1.2/flights/low-fare-search?
         // origin=${userFlightQuery.origin}
         // &destination=${userFlightQuery.destination}
