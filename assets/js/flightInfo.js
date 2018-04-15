3592c4a634fb128a20bbbec5e5cd39c04ac2ded6
walshcam <35083171+walshcam@users.noreply.github.com>
2018-04-15 12:08:40 -0400

    Created Future Adventures Page

--- assets/js/flightInfo.js
+++ assets/js/flightInfo.js
@@ -1,50 +1,31 @@
-let userFlightQuery = {
-    // Assigning Variables for the queryURL from targeted elements
-    // origin: $(),
-    // destination: $(),
-    // departureStartDate: $(),
-    // departureEndDate: $(),
-    // durationStart: $(),
-    // durationEnd: $(),
-    // maxPrice: $(),
-    // currency: $(),
-    // adults: $(),
-    // children: $(),
-    // infants: $(),
-    // nonstop: $(),
-    // travelClass: $(),
-    // numberOfResults: $(),
-    origin: "BOS",
-    destination: "LAX",
-    departureDate: "2018-06-25",
-    returnDate: "2018-07-15",
-    durationStart: 5,
-    durationEnd: 7,
-    maxPrice: "2000",
-    currency: "USD",
-    adults: 1,
-    children: 0,
-    infants: 0,
-    nonstop: true,
-    travelClass: "ECONOMY",
-    numberOfResults: 5,
+//GLOBAL VARIABLES
 
-    //function to create a request URL for the Amadeus API
-    flightQuery: function() {
-        var queryURL= `https://api.sandbox.amadeus.com/v1.2/flights/low-fare-search?apikey=p1YNpUhekCL0mknNyltGFNg8AAsc5yRI&origin=${userFlightQuery.origin}&destination=${userFlightQuery.destination}&departure_date=${userFlightQuery.departureDate}&return_date=${userFlightQuery.returnDate}&adults=${userFlightQuery.adults}&children=${userFlightQuery.children}&infants=${userFlightQuery.infants}&nonstop=${userFlightQuery.nonstop}&max_price=${userFlightQuery.maxPrice}&currency=${userFlightQuery.currency}&travel_class=${userFlightQuery.travelClass}&number_of_results=${userFlightQuery.numberOfResults}`;
-        // var queryURL = `https://api.sandbox.amadeus.com/v1.2/flights/low-fare-search?
-        // origin=${userFlightQuery.origin}
-        // &destination=${userFlightQuery.destination}
-        // &departure_date=&apikey=p1YNpUhekCL0mknNyltGFNg8AAsc5yRI`
-        // &duration=${userFlightQuery.durationStart}--${userFlightQuery.durationEnd}
-        // &max_price=${userFlightQuery.maxPrice}
-        // &currency=${userFlightQuery.currency}
-        // &adults=${userFlightQuery.adults}
-        // &children=${userFlightQuery.children}
-        // &infants=${userFlightQuery.infants}
-        // &nonstop=${userFlightQuery.nonstop}
-        // &travel_class=${userFlightQuery.travelClass}
-        // &number_of_results=${userFlightQuery.numberOfResults}
+let nonstop = "true";
+let roundtrip = true;
+
+$(document).ready(function() {
+    $("#searchAirline").on("click", function() {
+        let origin = $("#origin").val();
+        console.log("Origin before object: " + origin);
+
+        let destination = $("#destination").val();
+        let departureDate = $("#departDate").val();
+        let returnDate = $("#returnDate").val();
+        let currency = "USD";
+        let adults = $("#adults").val();
+        let children = $("#children").val();
+        let infants = $("#infants").val();
+        
+        travelClass = "ECONOMY";
+        numberOfResults = 1;
+
+        //function to create a request URL for the Amadeus API
+        if (roundtrip) {
+            var queryURL= `https://api.sandbox.amadeus.com/v1.2/flights/low-fare-search?apikey=p1YNpUhekCL0mknNyltGFNg8AAsc5yRI&origin=${origin}&destination=${destination}&departure_date=${departureDate}&return_date=${returnDate}&adults=${adults}&children=${children}&infants=${infants}&nonstop=${nonstop}&currency=${currency}&travel_class=${travelClass}&number_of_results=${numberOfResults}`;
+        }
+        else {
+            var queryURL= `https://api.sandbox.amadeus.com/v1.2/flights/low-fare-search?apikey=p1YNpUhekCL0mknNyltGFNg8AAsc5yRI&origin=${origin}&destination=${destination}&departure_date=${departureDate}&adults=${adults}&children=${children}&infants=${infants}&nonstop=${nonstop}&currency=${currency}&travel_class=${travelClass}&number_of_results=${numberOfResults}`;
+        }
 
         $.ajax({
             url: queryURL,
@@ -55,15 +36,60 @@ let userFlightQuery = {
             for (let i = 0; i < response.results.length; i++) {
                 //Return Price of Trip
                 let tripAirPrice = response.results[0].fare.total_price;
-                console.log(`The Price Of The Ticket is ${tripAirPrice}`);
+                $("#price").text(" $ " + tripAirPrice)
 
                 //Number of Layovers for Outbound Flight
                 let outboundFlightTotal = response.results[i].itineraries["0"].outbound.flights.length - 1;
-                console.log(`The number of layovers for the outbound flight is: ${outboundFlightTotal}`);
+                $("#outboundStops").text(outboundFlightTotal);
 
                 //Number of Layovers for Inbound Flight
                 let inboundFlightTotal = response.results[i].itineraries["0"].inbound.flights.length - 1;
-                console.log(`The number of layovers for the inbound flight is: ${inboundFlightTotal}`);
+                $("#inboundStops").text(inboundFlightTotal);
+
+                //*****************Date and Time for Overall Outbound Flights */
+
+                //Date for Outbound Flights
+
+                let outboundInitialDate = response.results[i].itineraries["0"].outbound.flights[0].departs_at.substring(0,10);
+                let outboundFinalDate = response.results[i].itineraries["0"].outbound.flights[0].arrives_at.substring(0,10);
+
+                if (outboundFinalDate === outboundInitialDate) {
+                    $("#outboundInitialDate").text(outboundInitialDate);
+                }
+                else {
+                    $("#outboundInitialDate").text(outboundInitialDate);
+                    $("#outboundFinalDate").text(outboundFinalDate);
+                }
+
+                //Time for Outbound Flights
+
+                let outboundInitialTime = response.results[i].itineraries["0"].outbound.flights[0].departs_at.substring(11,16);
+                let outboundFinalTime = response.results[i].itineraries["0"].outbound.flights[0].arrives_at.substring(11,16);
+
+                $("#outboundTime").text(outboundInitialTime + " - " + outboundFinalTime)
+                
+
+                //*****************Date and Time for Overall Inbound Flights */
+
+                //Date for Inbound Flights
+
+                let inboundInitialDate = response.results[i].itineraries["0"].inbound.flights[0].departs_at.substring(0,10);
+                let inboundFinalDate = response.results[i].itineraries["0"].inbound.flights[0].arrives_at.substring(0,10);
+
+                if (inboundFinalDate === inboundInitialDate) {
+                    $("#inboundInitialDate").text(inboundInitialDate);
+                }
+                else {
+                    $("#inboundInitialDate").text(inboundInitialDate)
+                    $("#inboundFinalDate").text(inboundFinalDate)    
+                }
+
+                //Time for Inbound Flights
+
+                let inboundInitialTime = response.results[i].itineraries["0"].inbound.flights[0].departs_at.substring(11,16);
+                let inboundFinalTime = response.results[i].itineraries["0"].inbound.flights[0].arrives_at.substring(11,16);
+
+                $("#inboundTime").text(inboundInitialTime + " - " + inboundFinalTime)
 
                 //OUTBOUND FOR LOOP <-THIS GIVES ARRIVAL TIMES AND DATES FOR EACH FLIGHT
                     //The for loop is in case of layovers
@@ -74,6 +100,10 @@ let userFlightQuery = {
                     let departureTime = departureRaw.substring(11,16);
                     let departureDate = departureRaw.substring(0,10);
                     let flightNumber = j+1;
+
+                    let operatingAirline = response.results[i].itineraries["0"].outbound.flights[j].operating_airline;
+
+                    console.log(`The operating airline is: ${operatingAirline}`)
                     console.log(`The departure time for the outgoing flight ${flightNumber} is: ${departureTime}`);
                     console.log(`The departure date for the outgoing flight ${flightNumber} is: ${departureDate}`);
                 
@@ -81,6 +111,7 @@ let userFlightQuery = {
                     let arrivalRaw = response.results[i].itineraries["0"].outbound.flights[j].arrives_at;
                     let arrivalTime = arrivalRaw.substring(11,16);
                     let arrivalDate = arrivalRaw.substring(0,10);
+
                     console.log(`The arrival time for the outgoing flight ${flightNumber} is: ${arrivalTime}`);
                     console.log(`The arrival date for the outgoing flight ${flightNumber} is: ${arrivalDate}`);
                 }
@@ -94,6 +125,10 @@ let userFlightQuery = {
                     let departureTime = departureRaw.substring(11,16);
                     let departureDate = departureRaw.substring(0,10);
                     let flightNumber = j+1;
+
+                    let operatingAirline = response.results[i].itineraries["0"].inbound.flights[j].operating_airline;
+
+                    console.log(`The operating airline is: ${operatingAirline}`)
                     console.log(`The departure time for the outgoing flight ${flightNumber} is: ${departureTime}`);
                     console.log(`The departure date for the outgoing flight ${flightNumber} is: ${departureDate}`);
                 
@@ -111,6 +146,41 @@ let userFlightQuery = {
         }).catch(function(err) {
             console.log(`There was an error processing the request. Desired price may be too low, the amount of people selected may be zero, or an input may be invalid`)
         });
-    }
-};
-userFlightQuery.flightQuery();
+    });
+});
+
+
+//*****BUTTON ANIMATION
+
+$('#oneWay').click(function() {
+    $('#roundTripSelector').animate({
+        'left': "50%"
+    });
+    roundTrip = false;
+    $("#hideReturnDate").hide();
+});
+$('#layovers').click(function() {
+    $('#nonStopSelector').animate({
+        'left': "50%"
+    });
+    nonstop = "false";
+});
+$('#roundTrip').click(function() {
+    $('#roundTripSelector').animate({
+        'left': "0"
+    });
+    roundTrip = true;
+    $("#hideReturnDate").show();
+});
+$('#nonStop').click(function() {
+    $('#nonStopSelector').animate({
+        'left': "0"
+    });
+    nonstop = "true";
+});
+
+//TEXTBOX Resizing
+function autoGrow(element) {
+    element.style.height = "100px";
+    element.style.height = (element.scrollHeight)+"px";
+}
