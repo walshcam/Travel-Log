035796b892f87449133396e3cb3d3e21fb2dd628
J H <jahalu88@hotmail.com>
2018-04-16 18:54:40 -0400

    trying moving map function onto one js file

--- assets/js/previousAdventures.js
+++ assets/js/previousAdventures.js
@@ -103,6 +103,61 @@ function querydatabase() {
             );
         });
 
+        var geocoder;
+        var map;
+        function initMap() {
+          geocoder = new google.maps.Geocoder();
+          var latlng = new google.maps.LatLng(-34.397, 150.644);
+          var mapOptions = {
+            zoom: 9,
+            center: latlng
+          }
+          map = new google.maps.Map(document.getElementById('map'), mapOptions);
+        };
+        
+        
+        function codeAddress() {
+          firebase.database().ref('/Posts/').once('value').then(function(snapshot) {
+            var postObject = snapshot.val();
+            console.log(postObject);
+            var keys = Object.keys(postObject);
+            // var currentRow;
+            // var Username = $("#name-input").val();
+            var mapArray=[];
+        
+            for (var i=0; i<keys.length; i++) {
+              var token = firebase.auth().currentUser.uid;
+              var currentObject = postObject[keys[i]];
+              if (token==currentObject.Username) {
+                  mapArray.push(currentObject)
+              }
+              };
+              console.log(mapArray);
+              for (j=0; j<mapArray.length; j++) {
+          var address = mapArray[j].location;
+          geocoder.geocode( { 'address': address}, function(results, status) {
+            if (status == 'OK') {
+              map.setCenter(results[0].geometry.location);
+              var marker = new google.maps.Marker({
+                  map: map,
+                  position: results[0].geometry.location
+                  
+              });
+            } else {
+              alert('Geocode was not successful for the following reason: ' + status);
+            }
+          });
+        };
+          });
+        };
+        
+
+
+
+
+
+
+
 
 
 
